01be08d55d35a9c8c1e538f5e298e007a03ecc2f
GD7 <GD7@DESKTOP-N5EMAV9>
2020-06-26 17:37:58 +0900

    [김민정] cafe24 메인 페이지 구현

---
+++ src/main/java/com/cafe24/alswjdd/alswjdd/AlswjddApplication.java
@@ -0,0 +1,13 @@
+package com.cafe24.alswjdd.alswjdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlswjddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlswjddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafe24/alswjdd/alswjdd/AlswjddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe24.alswjdd.alswjdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlswjddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
